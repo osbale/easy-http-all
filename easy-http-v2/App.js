02694c48bab9300d94996df083bf6cc0b7c6b42c
osbale <oskaras900@gmail.com>
2020-11-06 01:28:06 +0200

    Initial commit

---
+++ easy-http-v2/App.js
@@ -0,0 +1,34 @@
+const http = new EasyHTTP
+
+// Get Users
+
+/* const users = http.get('https://jsonplaceholder.typicode.com/users')
+.then(data => console.log(data))
+.catch(err => console.log(err)) */
+
+
+// User Data
+
+const data = {
+    name: 'John',
+    username: 'johndoa',
+    email: 'jdoe@gmail.com'
+}
+
+// Create Users
+
+/* http.post('https://jsonplaceholder.typicode.com/users', data)
+.then(data => console.log(data))
+.catch(err => console.log(err)) */
+
+// Update Users
+
+/* http.put('https://jsonplaceholder.typicode.com/users/2', data)
+.then(data => console.log(data))
+.catch(err => console.log(err)) */
+
+// Delete Users
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err))
